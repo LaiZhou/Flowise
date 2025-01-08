9145201d45c51d7698c7cc3b042c283a8accfbf9
Lai Zhou <hhlai1990@gmail.com>
2025-01-08 12:19:35 +0800

    fix md preProcess

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@
  #
  #```
 # pkill -9 -f "flowise" && kill -9 $(sudo lsof -i -P | grep LISTEN | grep :4000| grep 'node.*hello' | awk '{print $2}') && kill -9 $(sudo lsof -i -P | grep LISTEN | grep :8080| grep 'node.*hello' | awk '{print $2}')
-pnpm build
 pkill -9 -f "flowise" && netstat -anp | grep -Po ':4000\s.*LISTEN.*?\K\d+(?=/)' | xargs kill
+pnpm build
 pnpm start
