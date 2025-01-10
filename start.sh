77a8ab8ffd41b279042348ac021ac28880be5f63
Lai Zhou <hhlai1990@gmail.com>
2025-01-10 16:58:50 +0800

    fix ui

--- start.sh
+++ start.sh
@@ -5,5 +5,11 @@
  #```
 # pkill -9 -f "flowise" && kill -9 $(sudo lsof -i -P | grep LISTEN | grep :4000| grep 'node.*hello' | awk '{print $2}') && kill -9 $(sudo lsof -i -P | grep LISTEN | grep :8080| grep 'node.*hello' | awk '{print $2}')
 pkill -9 -f "flowise" && netstat -anp | grep -Po ':4000\s.*LISTEN.*?\K\d+(?=/)' | xargs kill
+git pull
+cd ../FlowiseChatEmbed
+git pull
+yarn install
+yarn build
+cd ../Flowise
 pnpm build
 pnpm start
